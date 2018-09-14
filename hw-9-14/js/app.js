f232901bf2162f4e9fae5e4a8c6c975ef0b765db
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-09-14 16:23:25 -0500

    Commit B

--- hw-9-14/js/app.js
+++ hw-9-14/js/app.js
@@ -1,1 +1,47 @@
-console.log('Hi')
+// A. Q + A
+// How do we assign a value to a variable?
+//---by setting it equal
+//
+// How do we change the value of a variable?
+//
+//
+// How do we assign an existing variable to a new variable?
+//---var car = "Ferrari"
+//
+//
+// Remind me, what are declare, assign, and define?
+//---declare: to give meaning to a variable
+//---assign: gives value to the variable
+//---define: describing what your function does
+//
+//
+// What is pseudocoding and why should you do it?
+//--The method of writing instuctions for you code that is easy to read and understand
+//
+// What percentage of time should be spent thinking about how you~re going to solve a problem vs actually typing in code to solve it?
+//--75% & 25%
+//
+//
+// B. Strings
+//
+// Create a variable called firstVariable.
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number.
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+// What is the value of firstVariable?
+var firstVariable = "Hello World";
+firstVariable = 9
+  console.log(firstVariable);
+
+var secondVariable = firstVariable
+  var secondVariable = firstVariable.toString();
+  console.log(secondVariable);
+
+
+  console.log(firstVariable);
+
+//
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+var yourName = "Ben Hankins";
+console.log('Hello, my name is ' +  yourName);
