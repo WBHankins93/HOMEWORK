64dfa259ef72e60190dc3c200790b3c6b9e83ac1
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-09-17 12:11:48 -0500

    HW 9-14

--- hw-9-14/js/app.js
+++ hw-9-14/js/app.js
@@ -20,8 +20,8 @@
 //
 // What percentage of time should be spent thinking about how you~re going to solve a problem vs actually typing in code to solve it?
 //--75% & 25%
-//
-//
+
+
 // B. Strings
 //
 // Create a variable called firstVariable.
@@ -45,3 +45,24 @@ var secondVariable = firstVariable
 // Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
 var yourName = "Ben Hankins";
 console.log('Hello, my name is ' +  yourName);
+
+
+
+C. Booleans
+Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console.
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a __ b);
+  console.log(c __ d);
+  console.log('Name' __ 'Name');
+  console.log(true __ false);
+  console.log(false __ false __ false __ false __ false __ false __ true);
+  console.log(false __ false)
+  console.log(e ___ 'Kevin');
+  console.log(a __ b ___ c);
+  console.log(a __ a ___ d);
+  console.log(48 __ '48');
