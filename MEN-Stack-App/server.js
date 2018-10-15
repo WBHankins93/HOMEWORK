3ca209ed8863aa89b3aba7696cf2dffc5587e181
Ben <WBHankins93@bens-air.home>
2018-10-14 19:50:18 -0500

    can now add to index

--- MEN-Stack-App/server.js
+++ MEN-Stack-App/server.js
@@ -19,9 +19,40 @@ app.get('/clubs', (req, res) => {
   })
 });
 
-// app.get('/clubs', (req,res) => {
-//   res.render('index.ejs', {clubs: Clubs})
-// });
+// Create a new route
+// make a new.ejs file
+// use app.get at default route
+// render new.ejs
+app.get('/clubs/new', (req, res) => {
+  res.render('new.ejs')
+});
+
+
+// use app.post at the route of new page
+// grab the body(info) of the form on new.ejs and console .log interval
+// push the body into your clubs models
+// redirect to your default route
+app.post('/clubs', (req, res) => {
+  console.log(req.body),
+  Clubs.push(req.body)
+  res.redirect('Clubs')
+});
+
+// use app.get at the route of the show page
+// show route by Index
+// render show route
+// check the index parameters of the model
+app.get('/clubs/:index', (req, res) => {
+  res.render('show.ejs', {
+    clubs: Clubs[req.params.index]
+  })
+});
+
+
+// use app.get at the route of the edit page
+
+
+
 
 
 app.listen(3000, (req, res) => {
