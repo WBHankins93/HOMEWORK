f4e0b5236b0e429ffa693f074eb34120b83c714f
Ben <WBHankins93@bens-air.home>
2018-10-14 23:26:35 -0500

    added a controller and converted to mongoose

---
+++ MEN-Stack-App/controllers/clubControl.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+
+// require the model for our Controller
+const Clubs = require('../models/clubs');
+
+// Need to add Index route here
+router.get('/', (req, res) => {
+  Clubs.find({}, (err, allClubs) => {
+    if (err) {
+     console.log(err);
+   } else {
+     console.log(allClubs)
+     res.render('index.ejs', {clubs: allClubs});
+   }
+ });
+});
+
+// Create a new route
+// make a new.ejs file
+// use app.get at default route
+// render new.ejs
+router.get('/new', (req, res) => {
+  res.render('new.ejs')
+});
+
+
+// use app.post at the route of new page
+// grab the body(info) of the form on new.ejs and console .log interval
+// push the body into your clubs models
+// redirect to your default route
+router.post('/', (req, res) => {
+  console.log(req.body),
+
+// Rewrite original code to use mongodb
+  Clubs.create(req.body, (err, newClubs) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(newClubs);
+      res.redirect('/clubs')
+    }
+  })
+});
+
+// use app.get at the route of the show page
+// show route by Index
+// render show route
+// check the index parameters of the model
+router.get('/:index', (req, res) => {
+  console.log(req.params);
+  Clubs.findById(req.params.index, (err, foundClubs) => {
+    // console.log(foundClubs, ' foundClubs')
+    res.render('show.ejs', {
+      clubs: foundClubs
+    })
+  })
+});
+
+
+// use app.get at the route of the edit page
+// show route by index and edit
+// render edit route
+router.get('/:index/edit', (req, res) => {
+  Clubs.findById(req.params.index, (err, foundClubs) => {
+    res.render('edit.ejs', {
+      clubs: foundClubs
+  })
+  })
+});
+
+
+// use app.put at the route of the default index
+// take the parameters of the model and set it equal to the body
+// redirect to the default route
+router.put('/:index', (req, res) => {
+  Clubs[req.params.index] = req.body;
+  res.redirect('/clubs');
+})
+
+
+module.exports = router;
--- MEN-Stack-App/server.js
+++ MEN-Stack-App/server.js
@@ -2,72 +2,25 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const Clubs = require('./models/clubs')
+
 
 // Require Mongoose database
 require('./db/db');
+
+// Require the Controller
+const clubsControl = require('./controllers/clubControl')
 
 // Middleware goes here
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
+// Always call the controller at the end of the middle ware
+// this will set up the controller route
+app.use('/clubs', clubsControl);
 
-// Need to add Index route here
-app.get('/clubs', (req, res) => {
-  res.render('index.ejs', {
-    clubs: Clubs
-  })
+app.get('/', (req, res) => {
+  res.send('Welcome to the Club')
 });
-
-// Create a new route
-// make a new.ejs file
-// use app.get at default route
-// render new.ejs
-app.get('/clubs/new', (req, res) => {
-  res.render('new.ejs')
-});
-
-
-// use app.post at the route of new page
-// grab the body(info) of the form on new.ejs and console .log interval
-// push the body into your clubs models
-// redirect to your default route
-app.post('/clubs', (req, res) => {
-  console.log(req.body),
-  Clubs.push(req.body)
-  res.redirect('Clubs')
-});
-
-// use app.get at the route of the show page
-// show route by Index
-// render show route
-// check the index parameters of the model
-app.get('/clubs/:index', (req, res) => {
-  res.render('show.ejs', {
-    clubs: Clubs[req.params.index]
-  })
-});
-
-
-// use app.get at the route of the edit page
-// show route by index and edit
-// render edit route
-app.get('/clubs/:index/edit', (req, res) => {
-  res.render('edit.ejs', {
-    clubs: Clubs[req.params.index],
-    index: req.params.index
-  })
-})
-
-
-// use app.put at the route of the default index
-// take the parameters of the model and set it equal to the body
-// redirect to the default route
-app.put('/clubs/:index', (req, res) => {
-  Clubs[req.params.index] = req.body;
-  res.redirect('/clubs');
-})
-
 
 app.listen(3000, (req, res) => {
   console.log('Ayy, Im workin here!!')
