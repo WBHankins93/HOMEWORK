d2c2b19e8475460c9e5fee627a9e5826f671a361
Ben <WBHankins93@bens-air.home>
2018-10-16 00:26:20 -0500

    route created in controller that sends to create user page

--- photo-site-app/controllers/users.js
+++ photo-site-app/controllers/users.js
@@ -6,8 +6,14 @@ router.get('/', (req, res) => {
   res.render('users/index.ejs');
 })
 
+// Make a route to add a new user
+// call from new.ejs
+router.get('/new', (req, res) => {
+  res.render('users/new.ejs');
+})
 
-
+// Create a route that will post the new user data
+// router.post()
 
 
 module.exports = router;
