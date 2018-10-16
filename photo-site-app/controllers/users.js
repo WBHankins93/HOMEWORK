dbc981a611e5e26ee6950e1b6cef4945a7368fb8
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-10-16 10:36:40 -0500

    created show page but still have to create route

--- photo-site-app/controllers/users.js
+++ photo-site-app/controllers/users.js
@@ -6,11 +6,16 @@ router.get('/', (req, res) => {
   res.render('users/index.ejs');
 })
 
+
 // Make a route to add a new user
 // call from new.ejs
 router.get('/new', (req, res) => {
   res.render('users/new.ejs');
 })
+
+
+// Make a show route here
+
 
 // Create a route that will post the new user data
 // Connect the model to the .create function and call in the body
